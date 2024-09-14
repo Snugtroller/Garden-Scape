c7d19439e420272b0310bc6ad09ae46a9aa205c2
Bhavneettt <bahuja9001996726@gmail.com>
2024-09-14 23:21:32 +0530

    desc

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css';
+import "./App.css";
 import Main from "./Components/Main";
-import GardenHeading from "./Components/GardenHeading"
+import GardenHeading from "./Components/GardenHeading";
+
 function App() {
   return (
     <div className="App">
-      <GardenHeading/>
-      <Main/>
+      <div className="heading-container">
+        <GardenHeading className="garden-heading" />
+      </div>
+      <div className="main-content">
+        <Main />
+      </div>
     </div>
   );
 }
--- src/Components/GardenHeading.jsx
+++ src/Components/GardenHeading.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import './GardenHeading.css';
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import "./GardenHeading.css";
 
 export default function AnimatedHeading() {
   const [isVisible, setIsVisible] = useState(true);
@@ -24,7 +24,40 @@ export default function AnimatedHeading() {
             transition={{ duration: 1 }}
             className="heading"
           >
-            GARDEN
+            <div
+              style={{
+                display: "flex",
+                flexDirection:'column',
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "justify",
+                margin: "0 auto",
+                pointerEvents: 'none',
+                
+              }}
+            >
+              Garden Scape
+            <motion.p
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
+            transition={{ duration: 1 }}
+              style={{
+                fontSize: 25,
+                textAlign: "justify",
+                margin: "0 auto",
+                width: "80%",
+                pointerEvents: 'none',
+                // zIndex: '-1',
+              }}
+            >
+              Welcome to our Virtual Herbal Garden project which offers an immersive and
+              educational platform that allows users to explore a diverse range
+              of medicinal plants used in AYUSH (Ayurveda, Yoga & Naturopathy,
+              Unani, Siddha, and Homeopathy).
+            </motion.p>
+            </div>
+
           </motion.h1>
         )}
       </AnimatePresence>
